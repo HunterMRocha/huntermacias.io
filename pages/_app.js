f51da34b281f24c990f68d12dba854a42ac78893
HunterMRocha <hmrocha2@dons.usfca.edu>
2022-12-17 08:30:03 -0800

    auth added for blogs

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,13 @@ import { SSRProvider } from 'react-bootstrap';
 // import ReactGA from "react-ga";
 // const TRACKING_ID = "G-GJM0P2R7PR";
 // ReactGA.initialize(TRACKING_ID);
-// import '@aws-amplify/ui-react/styles.css'
+import '@aws-amplify/ui-react/styles.css'
 import { AmplifyProvider } from '@aws-amplify/ui-react';
 // import Amplify from 'aws-amplify';
 
-// import config from './aws-exports';
+// import config from 'aws-exports';
 
-// Amplify.configure(config);
+// Auth.configure(config);
 
 // import ReactDOM from 'react-dom/client';
 
@@ -21,7 +21,7 @@ const App = ({ Component, pageProps }) => {
         <Component {...pageProps} />
       </ThemeProvider>
     
-     </AmplifyProvider>
+    </AmplifyProvider>
     
   );
 };
