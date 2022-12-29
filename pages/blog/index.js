7ec3e127293e0d15fa016d4da2e18ccdbebd7140
HunterMRocha <hmrocha2@dons.usfca.edu>
2022-12-29 03:50:34 -0800

    deploy issues

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -8,12 +8,11 @@ import Header from "../../components/Header";
 import data from "../../data/portfolio.json";
 import { ISOToDate, useIsomorphicLayoutEffect } from "../../utils";
 import { getAllPosts } from "../../utils/api";
-// import { Authenticator } from "aws-amplify/ui-react";
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css'
 import { Auth } from 'aws-amplify';
-
-import awsExports from '../../aws-exports';
+import { Image } from "@nextui-org/react";
+import awsExports from './aws-exports';
 
 Auth.configure(awsExports);
 
@@ -38,7 +37,7 @@ function Blog ({ posts }) {
   }, []);
 
   const createBlog = () => {
-    if (process.env.NODE_ENV === "development") {
+    // if (process.env.NODE_ENV != "development") {
       fetch("/api/blog", {
         method: "POST",
         headers: {
@@ -47,13 +46,13 @@ function Blog ({ posts }) {
       }).then(() => {
         router.reload(window.location.pathname);
       });
-    } else {
-      alert("This thing only works in development mode.");
-    }
+    // } else {
+    //   alert("This thing only works in development mode.");
+    // }
   };
 
   const deleteBlog = (slug) => {
-    if (process.env.NODE_ENV === "development") {
+    if (process.env.NODE_ENV != "development") {
       fetch("/api/blog", {
         method: "DELETE",
         headers: {
@@ -69,28 +68,26 @@ function Blog ({ posts }) {
       alert("This thing only works in development mode.");
     }
   };
+  
   // <Authenticator>
   return (
     showBlog.current && (
       <>
-        {data.showCursor && <Cursor />}
+        {/* {data.showCursor && <Cursor />} */}
         <Head>
           <title>Blog</title>
         </Head>
-        <div
-          className={`container mx-auto mb-10 ${
-            data.showCursor && "cursor-none"
-          }`}
-        >
+
+        <div className="container mx-auto mb-10">
           <Header isBlog={true}></Header>
           <div className="mt-10">
             <h1
               ref={text}
               className="mx-auto mob:p-2 text-bold text-6xl laptop:text-8xl w-full"
             >
-              Blog 
+              Tech-Stack 
             </h1>
-            <p className="mt-2 opacity-50 text-lg">compilation of articles </p>
+            <p className="mt-2 opacity-50 text-lg"></p>
             <div className="mt-10 grid grid-cols-1 mob:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 justify-between gap-10">
               {posts &&
                 posts.map((post) => (
@@ -99,17 +96,17 @@ function Blog ({ posts }) {
                     key={post.slug}
                     onClick={() => Router.push(`/blog/${post.slug}`)}
                   >
-                    <img
-                      className="w-full h-60 rounded-lg shadow-lg object-cover"
+                    <Image
+                      className="w-full h-60 rounded-lg border hover:border-emerald-300 shadow-lg object-cover"
                       src={post.image}
                       alt={post.title}
-                    ></img>
-                    <h2 className="mt-5 text-4xl">{post.title}</h2>
+                    ></Image>
+                    <h2 className="mt-5 text-emerald-300 font-thin text-2xl">{post.title}</h2>
                     <p className="mt-2 opacity-50 text-lg">{post.preview}</p>
                     <span className="text-sm mt-5 opacity-25">
                       {ISOToDate(post.date)}
                     </span>
-                    {process.env.NODE_ENV === "development" && mounted && (
+                    {process.env.NODE_ENV != "development" && mounted && (
                       <div className="absolute top-0 right-0">
                         <Button
                           onClick={(e) => {
@@ -127,7 +124,7 @@ function Blog ({ posts }) {
             </div>
           </div>
         </div>
-        {process.env.NODE_ENV === "development" && mounted && (
+        {/** process.env.NODE_ENV === "production"**/ true && mounted && (
           <div className="fixed bottom-6 right-6">
             <Button onClick={createBlog} type={"primary"}>
               Add New Post +{" "}
