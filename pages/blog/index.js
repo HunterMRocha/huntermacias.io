1e4a69466530579aa5c70d5fd95eac894755684b
HunterMRocha <hmrocha2@dons.usfca.edu>
2022-12-29 00:21:16 -0800

    cleaned up for deploy

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -13,7 +13,7 @@ import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css'
 import { Auth } from 'aws-amplify';
 import { Image } from "@nextui-org/react";
-import awsExports from './../../data/aws-exports';
+import awsExports from './aws-exports';
 
 Auth.configure(awsExports);
 
