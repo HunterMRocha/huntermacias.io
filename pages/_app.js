71dc3802022fe1b2a3ea023f5129b46eabad579c
HunterMRocha <hmrocha2@dons.usfca.edu>
2023-01-01 03:44:18 -0800

    testing meta data

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,19 @@ import { ThemeProvider } from "next-themes";
 import { SSRProvider } from 'react-bootstrap';
 import '@aws-amplify/ui-react/styles.css'
 import { AmplifyProvider } from '@aws-amplify/ui-react';
+import Head from "next/head";
 
 
 const App = ({ Component, pageProps }) => {
   return (
+    <div>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="titla, meta, nextjs" />
+        <meta name="author" content="Syamlal CM" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
     <SSRProvider>
 
       <AmplifyProvider>
@@ -17,6 +26,7 @@ const App = ({ Component, pageProps }) => {
       
       </AmplifyProvider>
     </SSRProvider>
+    </div>
     
   );
 };
